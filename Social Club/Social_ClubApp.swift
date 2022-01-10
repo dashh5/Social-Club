b3ff31511d26df8bdbaefc05e576c7eb9f05f231
Alexander Mehregan <53033599+amehregan10@users.noreply.github.com>
2022-01-09 21:51:38 -0800

    Initial Commit

---
+++ Social Club/Social_ClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_ClubApp.swift
+//  Social Club
+//
+//  Created by Alex M on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Social_ClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
